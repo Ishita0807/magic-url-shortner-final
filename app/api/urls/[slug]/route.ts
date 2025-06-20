8a9bcff0621553037cd243d3a592754ed0783aab
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2025-06-21 02:18:16 +0530

    Refactor GET function to improve parameter handling; removed params destructuring and streamlined slug extraction for clarity.

--- app/api/urls/[slug]/route.ts
+++ app/api/urls/[slug]/route.ts
@@ -2,8 +2,9 @@ import { prisma } from '@/lib/db'
 import { NextRequest, NextResponse } from 'next/server'
 
 
-export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
-  const data = await prisma.shortURL.findUnique({ where: { slug: params.slug } })
+export async function GET(req: NextRequest) {
+    const slug = req.nextUrl?.pathname.split('/').pop() || ''
+  const data = await prisma.shortURL.findUnique({ where: { slug: slug } })
     if (!data) return NextResponse.json({ error: 'URL not found' }, { status: 404 })
   return NextResponse.json(data)
 }
