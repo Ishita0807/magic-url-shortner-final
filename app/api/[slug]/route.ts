23861753c354e4a6668a831acb81fe031dc6c6ca
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2025-06-21 02:10:13 +0530

    Refactor GET function to improve type safety; changed params type to Record<string, string> and extracted slug variable for clarity.

--- app/api/[slug]/route.ts
+++ app/api/[slug]/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '../../../lib/db'
 
-export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
-  const data = await prisma.shortURL.findUnique({ where: { slug: params.slug } })
+export async function GET(req: NextRequest, { params }: { params: Record<string, string> }) {
+  const slug = await params.slug;
+  const data = await prisma.shortURL.findUnique({ where: { slug: slug } })
   if (!data) return NextResponse.redirect(new URL('/', req.url))
 
   await prisma.shortURL.update({ where: { slug: params.slug }, data: { clicks: { increment: 1 } } })
